a1a24a6fa72100ee312f0a8fa8f2f115b1aec079
ansht2000 <ansht2000@gmail.com>
2024-09-14 16:25:35 -0700

    added implementation for htmlnode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,35 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode("<p>", "Hello", ["hello"], {"test": "testing"})
+        node2 = HTMLNode("<p>", "Hello", ["hello"], {"test": "testing"})
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = HTMLNode("<p>", "Hello", ["hello"], {"test": "testing"})
+        node2 = HTMLNode("<p>", "Hello!", ["hello"], {"test": "testing"})
+        node3 = HTMLNode("<p>", "Hello!", ["hello", "goodbye"], {"test": "testing"})
+        node4 = HTMLNode("<p>", "Hello!", ["hello"], {"test": "testing", "testing": "one two three"})
+        node5 = HTMLNode("<h1>", "Hello", ["hello"], {"test": "testing"})
+        self.assertNotEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertNotEqual(node, node4)
+        self.assertNotEqual(node, node5)
+
+    def test_repr(self):
+        node = HTMLNode("<p>", "Hello", ["hello"], {"test": "testing"})
+        self.assertEqual("HTMLNode(<p>, Hello, ['hello'], {'test': 'testing'})", node.__repr__())
+
+    def test_props_to_html(self):
+        node = HTMLNode("<p>", "Hello", ["hello"], {"test": "testing"})
+        self.assertEqual("test=\"testing\"", node.props_to_html())
+        node2 = HTMLNode("<p>", "Hello!", ["hello"], {"test": "testing", "testing": "one two three"})
+        self.assertEqual("test=\"testing\" testing=\"one two three\"", node2.props_to_html())
+
+    def test_to_html(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -3,6 +3,7 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
+    
     def __eq__(self, other):
         return (
             self.text == other.text
@@ -11,5 +12,4 @@ class TextNode:
         )
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
     
