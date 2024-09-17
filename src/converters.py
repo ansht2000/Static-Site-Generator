42da0e43e61e8d7e2321e09f75c407b55200c4de
ansht2000 <ansht2000@gmail.com>
2024-09-17 00:48:16 -0700

    added implementantion of split_nodes_delimeter function

--- src/converters.py
+++ src/converters.py
@@ -1,5 +1,6 @@
 from textnode import TextNode
 from leafnode import LeafNode
+import re
 
 def text_node_to_html_node(text_node: TextNode):
     text_type = text_node.text_type
@@ -20,6 +21,40 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode(tag="img", value="", props={"src": url, "alt": text})
         case _:
             raise Exception("Invalid or unimplemented text node type")
-        
+
+# implementation using regexes
+# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+#     valid_delimiters = ["**", "*", "`"]
+#     if delimiter not in valid_delimiters:
+#         raise Exception("Invalid markdown syntax")
+#     new_nodes = []
+#     for node in old_nodes:
+#         node_text = node.text
+#         if node.text_type != "text":
+#             new_nodes.append(TextNode(node_text, node.text_type))
+#             continue
+#         delimited_text = re.findall(rf"\{delimiter}([^\{delimiter}]+)\{delimiter}", node_text)
+#         split_text = node_text.split(delimiter)
+#         nodes_to_add = map(lambda text: TextNode(text, text_type) if (text in delimited_text) else TextNode(text, "text"), split_text)
+#         new_nodes.extend(nodes_to_add)
+#     return new_nodes
+
+# implementation using map and filter
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    valid_delimiters = ["**", "*", "`"]
+    if delimiter not in valid_delimiters:
+        raise Exception("Invalid markdown syntax")
+    new_nodes = []
+    for node in old_nodes:
+        node_text = node.text
+        split_nodes = node_text.split(delimiter)
+        text_nodes = map(
+                lambda x: TextNode(x, "text")
+                if (split_nodes.index(x) % 2 == 0)
+                else TextNode(x, text_type), split_nodes
+        )
+        new_nodes.extend(list(filter(lambda x: x.text != "", text_nodes)))
+    return new_nodes
+
+
+        
--- src/test_converters.py
+++ src/test_converters.py
@@ -47,6 +47,34 @@ class TestConverter(unittest.TestCase):
             text_node_to_html_node(node)
         self.assertEqual(str(context.exception), "Invalid or unimplemented text node type")
 
+    def test_split_nodes_delimiter(self):
+        node = node = TextNode("This is text with a `code block` word", "text")
+        new_nodes = split_nodes_delimiter([node], "`", "code")
+        self.assertEqual([
+            TextNode("This is text with a ", "text"),
+            TextNode("code block", "code"),
+            TextNode(" word", "text"),
+        ], new_nodes)
+        node = node = TextNode("This is text with an *italic* word", "text")
+        new_nodes = split_nodes_delimiter([node], "*", "italic")
+        self.assertEqual([
+            TextNode("This is text with an ", "text"),
+            TextNode("italic", "italic"),
+            TextNode(" word", "text"),
+        ], new_nodes)
+        node = node = TextNode("This is text with an *italic*", "text")
+        new_nodes = split_nodes_delimiter([node], "*", "italic")
+        self.assertEqual([
+            TextNode("This is text with an ", "text"),
+            TextNode("italic", "italic"),
+        ], new_nodes)
+        node = node = TextNode("*italic* This is text with an", "text")
+        new_nodes = split_nodes_delimiter([node], "*", "italic")
+        self.assertEqual([
+            TextNode("italic", "italic"),
+            TextNode(" This is text with an", "text"),
+        ], new_nodes)
+
 
 
 if __name__ == "__main__":
