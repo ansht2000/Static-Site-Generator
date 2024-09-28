68011212c174c56830cbd1ef18751abb1fd8613c
ansht2000 <ansht2000@gmail.com>
2024-09-28 03:18:38 -0700

    moved all functions to the same file because of some circular referencing issues (to be fixed later) and implemented recursive generation of html sites

--- src/converters.py
+++ src/converters.py
@@ -1,6 +1,9 @@
 from leafnode import LeafNode
 from parentnode import ParentNode
 import re
+import os
+import shutil
+from pathlib import Path
 from textnode import (
     TextNode,
     text_type_text,
@@ -18,25 +21,51 @@ block_type_quote = "quote"
 block_type_unordered_list = "unordered_list"
 block_type_ordered_list = "ordered_list"
 
-def generate_page(from_path, template_path, to_path):
-    print(f"Generating page from {from_path} to {to_path} using {template_path}")
-    with open(from_path) as md_file:
-        markdown = md_file.read()
-    # not strictly necessary but we don't want a dangling fd
-    md_file.close()
-    with open(template_path) as temp_file:
-        template = temp_file.read()
-    temp_file.close()
-    title = extract_title_markdown(markdown)
-    html_node = markdown_to_html_node(markdown)
-    html = html_node.to_html()
-    html_file = template.replace("{{ Title }}", title)
-    html_file = html_file.replace("{{ Content }}", html)
-    with open(to_path, mode="w") as index_html:
-        print(html_file, file=index_html)
-    index_html.close()
-    
+def copy_content(src_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+    for filename in os.listdir(src_dir_path):
+        from_path = os.path.join(src_dir_path, filename)
+        to_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {to_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_content(from_path, to_path)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title_markdown(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
 
 def extract_title_markdown(markdown):
     header_one = re.findall(r"# (.*)", markdown)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
 import os
 import shutil
-from converters import generate_page
+from converters import generate_pages_recursive, copy_content
 
 dir_path_static = "./static"
 dir_path_public = "./public"
-
-def copy_content(src_dir_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-    for filename in os.listdir(src_dir_path):
-        from_path = os.path.join(src_dir_path, filename)
-        to_path = os.path.join(dest_dir_path, filename)
-        print(f" * {from_path} -> {to_path}")
-        if os.path.isfile(from_path):
-            shutil.copy(from_path, to_path)
-        else:
-            copy_content(from_path, to_path)
 
 def main():
     if not os.path.exists(dir_path_static):
@@ -28,6 +16,7 @@ def main():
         os.mkdir(dir_path_public)
     print("Copying static files to public directory...")
     copy_content(dir_path_static, dir_path_public)
-    generate_page("./content/index.md", "template.html", "./public/index.html")
+    print("Generating content...")
+    generate_pages_recursive("./content", "template.html", "./public")
 
 main()
